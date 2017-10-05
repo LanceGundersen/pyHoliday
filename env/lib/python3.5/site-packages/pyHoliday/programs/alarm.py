edb53ab16bc1ce702c875383be9fa39141e73b08
Lance <gundersen.lance@gmail.com>
2017-10-05 01:16:39 -0400

    Reconfigure Project Folder Structure

---
+++ env/lib/python3.5/site-packages/pyHoliday/programs/alarm.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    Halloween Alarm and Light Program aka alarm.
+
+    This file controls all aspects of the alarm program. It has the unique
+    scripts to run on linux and chip at the moment. Linux doesn't have the
+    light/relay trigger based on hardware limitations as I know it.
+
+    Ideas:
+    - Look into if the nested if/while statements are the best way to run this
+    program
+    - Add Pi Support
+    - For Linux/PC add Arduino support for relays
+    - For Linux/PC add wireless WiFi Relay Support
+"""
+
+import pyHoliday.functions as f
+import pyHoliday.init as i
+import time
+
+
+def linux():
+    f.playAudio(i.alarm)
+    f.playAudio(i.voice)
+    return
+
+
+def chip():
+    import CHIP_IO.GPIO as GPIO
+    trigger = i.cfg['chip']['trigger_pin']
+    light = i.cfg['chip']['relay_pin']
+    GPIO.setup(trigger, GPIO.OUT)
+    GPIO.setup(light, GPIO.OUT)
+    GPIO.output(light, GPIO.HIGH)
+    f.playAudio(i.intro)
+    f.playAudio(i.alarm)
+    f.playAudio(i.voice)
+    GPIO.output(light, GPIO.LOW)
+    return
+
+
+def alarm():
+    f.playAudio(i.intro)
+    if(i.platform == 'Linux' or 'linux'):
+        if(i.continuousRun):
+            while(i.continuousRun):
+                linux()
+                time.sleep(i.sleep)
+        else:
+            linux()
+        return
+    elif(i.platform == 'Chip' or 'chip'):
+        if(i.continuousRun):
+            while(i.continuousRun):
+                chip()
+                time.sleep(i.sleep)
+        else:
+            chip()
---
+++ env/lib/python3.5/site-packages/pyHoliday/pyHoliday.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    Entry file for pyHoliday.
+
+    This file displays the current configuration and allows the users to select
+    a program from the list.
+
+    Ideas:
+    - Look at the programs directory and return the list to be selected.
+    - Allow the user to edit the config.yml here.
+"""
+
+import pyHoliday.programs.alarm as a
+import pyHoliday.init as i
+
+
+def config():
+    print('Continuous Running: ' + str(i.continuousRun))
+    print('Time Between Running: ' + str(i.sleep))
+    print('Platform: ' + i.platform)
+    return
+
+
+print('\nWelcome to pyHoliday!\n')
+config()
+print('Continue? Y/n')
+c = input()
+if(c == 'y' or 'Y' or 'yes' or 'Yes' or 'YES' or ''):
+    print('Select Program from list: Enter the number:\n')
+    print('1. Light & Audio Trigger')
+    p = input()
+    if(p == '1'):
+        print('Program 1 selected, enjoy!')
+        a.alarm()
+    else:
+        print('No program selected, exiting...')
+elif(c == 'n' or 'N' or 'No' or 'NO'):
+    print('Exiting..')
